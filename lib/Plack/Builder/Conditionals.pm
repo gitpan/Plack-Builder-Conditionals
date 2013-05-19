4082ff232688b730b8e8d13d6a7ae596dcdbb7ac
Masahiro Nagano <kazeburo@gmail.com>
2013-05-19 09:21:30 +0000

    Import of KAZEBURO/Plack-Builder-Conditionals-0.05 from CPAN.
    gitpan-cpan-distribution: Plack-Builder-Conditionals
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         KAZEBURO/Plack-Builder-Conditionals-0.05.tar.gz
    gitpan-cpan-author:       KAZEBURO
    gitpan-cpan-maturity:     released

--- lib/Plack/Builder/Conditionals.pm
+++ lib/Plack/Builder/Conditionals.pm
@@ -7,7 +7,7 @@ use List::MoreUtils qw//;
 use Plack::Util;
 use Plack::Middleware::Conditional;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 sub import {
     my $class = shift;
@@ -138,7 +138,6 @@ sub header {
 }
 
 sub browser {
-    my $header = shift;
     _match( "HTTP_USER_AGENT", @_ );
 }
 
